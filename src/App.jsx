638961bfa6d41ec71e7401f4cae1850c6db40463
Atharv Kulkarni <atkulkar@redhat.com>
2025-06-20 14:24:50 +0530

    ci(deployment): added deployment process

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <CartProvider>
-     <Router>
+     <Router basename="/tryflix">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/checkout" element={<CheckoutForm />} />
